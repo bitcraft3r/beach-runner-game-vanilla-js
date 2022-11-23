fb145b067c22312e9f9b478241622aca1a95b1b4
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-23 14:35:09 +0800

    add use spacebar to jump

--- helpers/playerStates.js
+++ helpers/playerStates.js
@@ -50,7 +50,7 @@ export class Running extends State {
         this.game.particles.push(new Dust(this.game, this.game.player.x + this.game.player.width * 0.6, this.game.player.y + this.game.player.height));
         if (input.includes(`ArrowDown`)){
             this.game.player.setState(states.IDLE, 0);
-        } else if (input.includes(`ArrowUp`)){
+        } else if (input.includes(`ArrowUp`) || input.includes(` `)){
             this.game.player.setState(states.JUMPING, 1);
         } 
         // else if (input.includes('Enter')){
